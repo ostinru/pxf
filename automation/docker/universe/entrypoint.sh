a84bb5f51b3cfc47d5d04ca224ba15333b870870
Nikolay Antonov <ostinru@gmail.com>
2025-08-01 00:39:19 +0500

    configure ssh

--- automation/docker/universe/entrypoint.sh
+++ automation/docker/universe/entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -e
 
+
+# ----------------------------------------------------------------------
+# Start SSH daemon and setup for SSH access
+# ----------------------------------------------------------------------
+# The SSH daemon is started to allow remote access to the container via
+# SSH. This is useful for development and debugging purposes. If the SSH
+# daemon fails to start, the script exits with an error.
+# ----------------------------------------------------------------------
+if [ ! -d /var/run/sshd ]; then
+   sudo mkdir /var/run/sshd
+   sudo chmod 0755 /var/run/sshd
+fi
+if ! sudo /usr/sbin/sshd; then
+    echo "Failed to start SSH daemon"
+    exit 1
+fi
+
+# ----------------------------------------------------------------------
+# Remove /run/nologin to allow logins for all users via SSH
+# ----------------------------------------------------------------------
+sudo rm -rf /run/nologin
+
 # --------------------------------------------------------------------
 # Run tests
 # --------------------------------------------------------------------
